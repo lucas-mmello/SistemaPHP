b44de3fc031891acced82eeae3cff8db19b5735c
lucas mello <lucasmello03@hotmail.com>
2023-05-02 21:05:34 -0300

    adicionei o menu nas telas menores que 900px e arrumei um pouco a responsividade

--- js/index.js
+++ js/index.js
@@ -1,9 +1,25 @@
 // Altera os temas claro e escuro e as imagens
 
-const themeToggle = document.getElementById("theme-toggle");
+const themeToggle = document.querySelectorAll(".theme-toggle");
 const root = document.documentElement;
 
-themeToggle.addEventListener("click", (e) => {
-  e.preventDefault();
-  root.classList.toggle("light");
+themeToggle.forEach((themeToggle) => {
+  themeToggle.addEventListener("click", (e) => {
+    e.preventDefault();
+    root.classList.toggle("light");
+  });
 });
+
+//icone menu
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".nav");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
--- page.php
+++ page.php
@@ -2,8 +2,9 @@
   <body>
     <!-- Menu lateral -->
     <div class="sidebar">
-      <nav>
-        <a href="" id="theme-toggle" class="nav-link">Mudar Tema</a>
+
+      <nav >
+        <a href="" class="theme-toggle" class="nav-link">Mudar Tema</a>
         <a href="welcomepage.php" class="nav-link">Página Inicial</a>
         <a href="form_aluno.php" class="nav-link">Administração de alunos</a>
         <a href="" class="nav-link">Administração de logins</a>
@@ -13,6 +14,23 @@
         <a href="" class="nav-link">Sair</a>
       </nav>
     </div>
+    <header class="header">
+      
+      <i
+        class="bx bx-menu "
+        id="menu-icon"
+      ></i>
+      <nav class="nav">
+        <a href="" class="theme-toggle" class="nav-link">Mudar Tema</a>
+        <a href="welcomepage.php" class="nav-link">Página Inicial</a>
+        <a href="form_aluno.php" class="nav-link">Administração de alunos</a>
+        <a href="" class="nav-link">Administração de logins</a>
+        <a href="" class="nav-link">Administração de materias</a>
+        <a href="" class="nav-link">Lançamento de Notas</a>
+        <a href="" class="nav-link">Relatório de notas</a>
+        <a href="" class="nav-link">Sair</a>
+      </nav>
+    </header>
     <!-- Parte principal -->
     <div class="main">
       <main>
