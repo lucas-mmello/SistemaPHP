b43772b45b3a2e1d98a9fd0858148eb553dc33cd
lucas mello <lucasmello03@hotmail.com>
2023-05-12 11:59:42 -0300

    adicionando estilo no erro do login

--- js/login.js
+++ js/login.js
@@ -1,9 +1,20 @@
 // tema selecionado
 const root = document.documentElement;
+const url = window.location.href;
 document.addEventListener("DOMContentLoaded", () => {
   const theme = localStorage.getItem("theme");
   if (theme === "light") {
     root.classList.add("light");
+  }
+  if (url.includes("erro")) {
+    document.querySelectorAll("input").forEach((inp) => {
+      inp.classList.add("shake-horizontal");
+    });
+    setTimeout(() => {
+      document.querySelectorAll("input").forEach((inp) => {
+        inp.classList.remove("shake-horizontal");
+      });
+    }, 1200);
   }
 });
 
