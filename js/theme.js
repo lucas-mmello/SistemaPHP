2cddf7318626f6f2b0ae2a77d034b5ca5086ba0d
lucas mello <lucasmello03@hotmail.com>
2023-05-24 15:45:21 -0300

    corrigindo mais um bg

--- js/theme.js
+++ js/theme.js
@@ -7,6 +7,9 @@
   if (theme === "light") {
     root.classList.add("light");
   }
+  if (window.location.href.includes(".php") === false) {
+    window.location.href += "index.php";
+  }
   if (window.location.href.includes("index.php") === false) {
     const side = localStorage.getItem("sidebar");
     if (side === "small") {
