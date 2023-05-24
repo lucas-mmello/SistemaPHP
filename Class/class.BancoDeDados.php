50529d7a0c52120eb4dae0ef33f62c3ac219822b
lucas mello <lucasmello03@hotmail.com>
2023-05-24 19:34:36 -0300

    aula

---
+++ Class/class.BancoDeDados.php
@@ -0,0 +1,23 @@
+<?php
+
+class BancoDeDados{
+  private $host;
+  private $usuario;
+  private $senha;
+  private $banco;
+  private $conexao;
+
+  public function __construct($host = 'localhost',
+  $usuario = 'root', $senha = '', $banco = 'aedb_quinto')
+  {
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+    $this->host = $host;
+    $this->usuario = $usuario;
+    $this->senha = $senha;
+    $this->banco = $banco;
+  }
+  public function conectar(){
+    $con = mysqli_connect($this->host, $this->usuario, $this->senha) or die('Erro na conexão');
+  }
+}
