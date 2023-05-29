b9605afd987c5cab39dac87ef8361d98f4df47a8
lucas mello <lucasmello03@hotmail.com>
2023-05-29 19:09:38 -0300

    adicionei o typed no h2 do relatorio de notas

--- pageRelatorio.php
+++ pageRelatorio.php
@@ -13,7 +13,7 @@
     ?>
     <div class="content">
         <div class="center">
-            <h2>Relatório de Notas</h2>
+            <h2><span class="multiple-text"></span></h2>
             <form action="class/class.Relatorio.php" method="post">
                 <div class="inp-group">
                     <select name="idaluno" class="input" id="">
@@ -36,4 +36,18 @@
     </div>
 
 <?php require_once("footer.php") ?>
+<script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const element = document.querySelector(".multiple-text");
+            if (element) {
+                const typed = new window.Typed(element, {
+                    strings: ["Relatório de Notas"],
+                    typeSpeed: 100,
+                    backSpeed: 100,
+                    backDelay: 5000,
+                    loop: false,
+                });
+            }
+        });
+    </script>
 </body>
